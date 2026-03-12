2f0a66930fab563f33a490a4cdf9f719dfadf9b3
Jonathan Hallström <lmj.hallstrom@gmail.com>
2026-03-12 22:33:50 +0100

    idea from truecumber in TCEC chat, use halfmove hash for corrhist indexing
    bench: 3368624

--- src/search.rs
+++ src/search.rs
@@ -1292,10 +1292,10 @@ fn eval_correction(td: &ThreadData, ply: isize) -> i32 {
     let stm = td.board.side_to_move();
     let corrhist = td.corrhist();
 
-    (corrhist.pawn.get(stm, td.board.pawn_key())
-        + corrhist.minor.get(stm, td.board.minor_key())
-        + corrhist.non_pawn[Color::White].get(stm, td.board.non_pawn_key(Color::White))
-        + corrhist.non_pawn[Color::Black].get(stm, td.board.non_pawn_key(Color::Black))
+    (corrhist.pawn.get(stm, td.board.pawn_key() ^ td.board.halfmove_hash())
+        + corrhist.minor.get(stm, td.board.minor_key() ^ td.board.halfmove_hash())
+        + corrhist.non_pawn[Color::White].get(stm, td.board.non_pawn_key(Color::White) ^ td.board.halfmove_hash())
+        + corrhist.non_pawn[Color::Black].get(stm, td.board.non_pawn_key(Color::Black) ^ td.board.halfmove_hash())
         + td.continuation_corrhist.get(
             td.stack[ply - 2].contcorrhist,
             td.stack[ply - 1].piece,
@@ -1314,11 +1314,11 @@ fn update_correction_histories(td: &mut ThreadData, depth: i32, diff: i32, ply: 
     let corrhist = td.corrhist();
     let bonus = (142 * depth * diff / 128).clamp(-4923, 3072);
 
-    corrhist.pawn.update(stm, td.board.pawn_key(), bonus);
-    corrhist.minor.update(stm, td.board.minor_key(), bonus);
+    corrhist.pawn.update(stm, td.board.pawn_key() ^ td.board.halfmove_hash(), bonus);
+    corrhist.minor.update(stm, td.board.minor_key() ^ td.board.halfmove_hash(), bonus);
 
-    corrhist.non_pawn[Color::White].update(stm, td.board.non_pawn_key(Color::White), bonus);
-    corrhist.non_pawn[Color::Black].update(stm, td.board.non_pawn_key(Color::Black), bonus);
+    corrhist.non_pawn[Color::White].update(stm, td.board.non_pawn_key(Color::White) ^ td.board.halfmove_hash(), bonus);
+    corrhist.non_pawn[Color::Black].update(stm, td.board.non_pawn_key(Color::Black) ^ td.board.halfmove_hash(), bonus);
 
     if td.stack[ply - 1].mv.is_some() && td.stack[ply - 2].mv.is_some() {
         td.continuation_corrhist.update(
